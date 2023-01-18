208a4eea8f4aaee2a8d3cf2ae2f2dcf0c7a7708d
koba <kobagogoberidze21@gmail.com>
2023-01-18 22:37:08 +0400

    Implement description

---
+++ .description.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use Bitrix\Bizproc\FieldType;
+
+$arActivityDescription = [
+    "NAME" => GetMessage("JC_GWC_NAME"),
+    "DESCRIPTION" => GetMessage("JC_GWC_DESCRIPTION"),
+    "TYPE" => array("activity"),
+    "CLASS" => "JCGetWorkflowsCountActivity",
+    "JSCLASS" => "BizProcActivity",
+    "CATEGORY" => array(
+        "ID" => "other",
+    ),
+    "RETURN" => array(
+        "Count" => array(
+            "NAME" => GetMessage("JC_GWC_RETURN_COUNT_FIELD_TITLE"),
+            "TYPE" => FieldType::INT,
+        ),
+    ),
+];
---
+++ lang/en/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS['JC_GWC_NAME'] = "Get active workflow instances count";
+$MESS['JC_GWC_DESCRIPTION'] = "Get active workflow instances count";
+$MESS['JC_GWC_RETURN_COUNT_FIELD_TITLE'] = "Workflow instances count";
